64f5753bff23390557f97ec48a97417751150030
j0590 <jacksimpson0590@gmail.com>
2025-12-13 02:51:19 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -15,6 +15,7 @@ dltoken(){ url="$1"; out="$2"; hdr="$3"; if [ -f "$out" ]; then echo "OK: $out";
 dl "https://huggingface.co/FX-FeiHou/wan2.2-Remix/resolve/main/NSFW/Wan2.2_Remix_NSFW_i2v_14b_high_lighting_v2.0.safetensors" "$WORKDIR/models/diffusion_models/Wan2.2_Remix_NSFW_i2v_14b_high_lighting_v2.0.safetensors"
 dl "https://huggingface.co/FX-FeiHou/wan2.2-Remix/resolve/main/NSFW/Wan2.2_Remix_NSFW_i2v_14b_low_lighting_v2.0.safetensors" "$WORKDIR/models/diffusion_models/Wan2.2_Remix_NSFW_i2v_14b_low_lighting_v2.0.safetensors"
 dl "https://huggingface.co/NSFW-API/NSFW-Wan-UMT5-XXL/resolve/main/nsfw_wan_umt5-xxl_fp8_scaled.safetensors" "$WORKDIR/models/clip/nsfw_wan_umt5-xxl_fp8_scaled.safetensors"
+dl "https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/3902f61b9647e65d4de4fb5febad34ca0e4dc5c8/split_files/vae/wan_2.1_vae.safetensors" "$WORKDIR/models/vae/wan_2.1_vae.safetensors"
 if [ ! -f "$WORKDIR/models/upscale_models/1xSkinContrast-SuperUltraCompact.pth" ]; then gdown --id 1-pC6_7Lrmy3p-VAh-dGzvETRBUUAQzmV -O "$WORKDIR/models/upscale_models/1xSkinContrast-SuperUltraCompact.pth"; fi
 if [ -n "${civitai_token:-}" ]; then dltoken "https://civitai.com/api/download/models/2073605" "$WORKDIR/models/loras/nsfwsks_high.safetensors" "Authorization: Bearer ${civitai_token}"; dltoken "https://civitai.com/api/download/models/2083303" "$WORKDIR/models/loras/nsfwsks_low.safetensors" "Authorization: Bearer ${civitai_token}"; fi
 cd "$WORKDIR"
